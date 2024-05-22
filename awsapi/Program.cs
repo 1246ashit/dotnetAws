6e634472b18c6ebbcf869252b35d56be99ede57f
jj <1246ashit@gmail.com>
2024-05-22 22:09:59 +0800

    docker-compose 新增postgresql 建立

--- awsapi/Program.cs
+++ awsapi/Program.cs
@@ -1,10 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //cros
-var awsApi = "awsApi";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: awsApi,
+    options.AddPolicy(name: "awsApi",
                       policy  =>
                       {
                           policy.AllowAnyOrigin()
@@ -30,29 +29,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
